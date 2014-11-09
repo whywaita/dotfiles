1b1601c6345b0390ffdc7b856af18c34fdefe5bc
whywaita <nikoniko.why+github@gmail.com>
2014-11-10 04:52:16 +0900

    fix typo
    ShellScriptの記法的に崩れてたから直した

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd $(dirname $0)
+cd ${dirname $0}
 for dotfile in .?*
 do
     if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
@@ -18,7 +18,7 @@ rmdir *
 echo "install rbenv? (yes/no)"
 read rbenvs
 
-case "${rbenvs}" in
+case "${rbenv}" in
 	yes)
 	git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
 	mkdir -p $HOME/.rbenv/plugins
