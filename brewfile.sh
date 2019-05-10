62615cbd859edb354302d2f7f48030a2724b0336
whywaita <whywaita@whywrite.it>
2019-05-10 11:14:46 +0900

    update modern homebrew packages

--- brewfile.sh
+++ brewfile.sh
@@ -16,7 +16,6 @@ brew install readline
 
 #git
 brew install git
-brew install git-flow
 #brew install tig
 
 #font
@@ -26,60 +25,32 @@ brew tap sanemat/font
 #tools
 brew install tmux
 brew install nkf
-brew install vim --with-lua
+brew install vim 
 brew install w3m
 brew install curl
 brew install wget
 brew install tree
 brew install nmap
-## TeX
-#brew install ghostscript
-#brew install imagemagick
-## movie
-#brew install ffmpeg
-#brew install mplayer --use-gcc
-#brew install aalib
-#brew install libcasa --use-gcc
-## peco
+
+# dev
 brew install go
-brew tap peco/peco
-brew install peco
+brew install nvim
+brew install kubectl
+brew install gpg2
 
 ## Homebrew-cask
 brew tap caskroom/cask
 
 brew cask install iterm2
-#brew cask install android-studio
-#brew cask install handbrake
-#brew cask install audacity
 brew cask install vlc
-#brew cask install thunderbird
-#brew cask install evernote
-#brew cask install dropbox
 brew cask install coteditor
-brew cask install growlnotify
-#brew cask install limechat
-#brew cask install sublime-text
-#brew cask install skype
-#brew cask install yorufukurou
 brew cask install alfred
 brew cask install pandoc
 brew cask install adobe-reader
-#cask install wiresheke
 brew cask install xquartz
-#brew cask install basictex
-#brew cask install mac-linux-usb-loader
-#brew cask install virtualbox
-#brew cask install vagrant
-#brew cask install hipchat
-
-# install java
-#brew tap caskroom/versions
-
-#brew cask install java7
-#brew cask install java6
-
-brew cask alfred link
+brew cask install karabiner-elements
+brew cask install slack
+brew cask install 1password
 
 #remove dust
 brew cleanup
