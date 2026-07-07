1f8e64c1a0b7facc1b8d9886ad35955422d66740
whywaita <whywaita@whywrite.it>
2026-07-07 15:06:04 +0900

    fix: recover interrupted dein clone, pass wheel-up to mouse-aware apps, drop dead wezterm config

--- scripts/setup-nvim.sh
+++ scripts/setup-nvim.sh
@@ -5,6 +5,10 @@ DEIN_DIR="$HOME/.cache/dein/repos/github.com/Shougo/dein.vim"
 if [ -d "$DEIN_DIR/.git" ]; then
   echo "Skip: dein.vim already cloned"
 else
+  if [ -d "$DEIN_DIR" ]; then
+    echo "Removing incomplete dein.vim directory (no .git found)"
+    rm -rf "$DEIN_DIR"
+  fi
   mkdir -p "$HOME/.cache/dein/repos/github.com/Shougo"
   git clone https://github.com/Shougo/dein.vim.git "$DEIN_DIR"
 fi
